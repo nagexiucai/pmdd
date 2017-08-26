df61fd01fe92d6ca3d8a1c4d578a34cb669c6976
nagexiucai <nagexiucai@qq.com>
2017-08-26 12:57:15 +0800

    修改md-maps.json中JSON格式错误； 添加简单分析逻辑； 发现处理“from x.y import z”中“x.y”模块__spec__.origin有问题。

--- aio.py
+++ aio.py
@@ -10,7 +10,9 @@ import sys
 import os
 import atexit
 import importlib
+import json
 from types import ModuleType
+from collections import Counter
 from cfg import mprint, mpprint
 
 __out__ = {}
@@ -99,14 +101,18 @@ def rinse(entry):
                 elif _ is None:
                     mprint("@", "import error.")
                 else:
-                    if line.startswith("from "):
-                        _, __ = line.split(" import ")
-                    else:
-                        _, __ = line.split("import ")
-                    if _:
-                        _, package = _.split()
-                    else:
-                        package = ""
+                    try:
+                        if line.startswith("from "):
+                            _, __ = line.split(" import ")
+                        else:
+                            _, __ = line.split("import ")
+                        if _:
+                            _, package = _.split()
+                        else:
+                            package = ""
+                    except ValueError:
+                        mprint("@", "unsupported now")
+                        continue
                     phrases = [[__.strip() for __ in _.strip().split("as")] for _ in __.split(",")]
                     if package:
                         if package.startswith("."):
@@ -115,7 +121,7 @@ def rinse(entry):
                         if m:
                             owner = _do_owner(m)
                             mprint("$", package, owner)
-                            md[package] = {"user": [os.path.basename(entry)], "owner": owner}
+                            md[package] = {"user": [entry], "owner": owner}
                         else:
                             mprint("@", "uh-huh")
                     for phrase in phrases:
@@ -130,7 +136,7 @@ def rinse(entry):
                         if m:
                             owner = _do_owner(m)
                             mprint("$", absolute, owner)
-                            md[absolute] = {"user": [os.path.basename(entry)], "owner": owner}
+                            md[absolute] = {"user": [entry], "owner": owner}
     return md
 
 
@@ -166,7 +172,7 @@ def tree(entry, out={}):
         if _.endswith(__):  # TODO: 似乎直接用entry不需要取出文件名
             return
     md = rinse(entry)
-    for k,v in md.items():
+    for k, v in md.items():
         vv = out.get(k)
         if vv is not None:
             vv["user"].extend(v.get("user"))
@@ -177,8 +183,42 @@ def tree(entry, out={}):
     for _, information in md.items():
         tree(information.get("owner"), out)
 
+
+def analyse(jf):
+    sb = open(jf, encoding="utf-8")
+    jj = json.load(sb)
+    sb.close()
+    _ = []
+    __ = []
+    ___ = []
+    opposite = {}
+    for k, v in jj.items():
+        _.append(k)
+        __.append(v.get("owner"))
+        user = v.get("user")
+        assert len(user) == len(set(user))
+        ___.extend(user)
+        for u in user:
+            if u in opposite:  # TODO: py3没有dict.has_key
+                opposite[u].append(k)
+            else:
+                opposite[u] = [k]
+    counter = Counter(__)
+    mprint("$", "modules in total", len(_), "have not repetition", len(_) == len(set(_)))
+    mprint("$", "owners in total", len(__), "have not repetition", len(__) == len(set(__)), "they are", [i for i in filter(lambda _: counter.get(_) > 1, counter)])  # TODO: filter在py3中返回生成器，在py2中返回和数据源容器类型相同
+    mprint("$", "users in total", len(set(___)), "but import", len(___), "times in total")
+    counter = Counter(___)
+    mprint("$", "users repetition >>>>>")
+    mpprint([i for i in filter(lambda _: counter.get(_) > 1, counter)])
+    mprint("$", "opposite sees >>>>>")
+    mpprint(opposite)
+    mprint("$", "this project depends on >>>>")
+    mpprint(dict([(i, opposite.get(i)) for i in filter(lambda _: _.startswith("E:\\nagexiucai\\pmdd"), opposite)]))
+
+
 if __name__ == "__main__":
-    tree("./test/entry.py", __out__)
+    # tree("./test/entry.py", __out__)
     # mprint(dir())
     # mprint(globals())
     # mprint(locals())
+    analyse("./md-maps-plus.json")
